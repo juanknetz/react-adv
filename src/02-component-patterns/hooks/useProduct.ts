6f6e84273b28d115c881274dab3c57a8bd0d39be
Juan Carlos Netz Escalante <jcnetz@hotmail.com>
2023-10-18 17:05:37 -0400

    Control props alt

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -13,13 +13,8 @@ export const useProduct = ({
   value = 0,
 }: useProductArgs) => {
   const [counter, setCounter] = useState(value);
-  const isControlled = useRef(!!onChange);
 
   const increaseBy = (value: number) => {
-    if (isControlled) {
-      return onChange!({ count: value, product });
-    }
-
     const newValue = Math.max(counter + value, 0);
     setCounter(newValue);
     
